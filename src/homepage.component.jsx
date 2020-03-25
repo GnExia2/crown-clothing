4a1462858e5b88b64a50b0ff09c6edd57f59ed4e
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-25 13:18:23 -0700

    created text for initial homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
 const HomePage = () => (
-<<<<<<< HEAD
-  <div className="">
-    
+  <div className="homePage">
+    <div className = "directory-menu">
+      <div className = "menu-item">
+        <div className = "content">
+          <h1 className = "title">HATS</h1>
+          <span className = "subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className = "menu-item">
+        <div className = "content">
+          <h1 className = "title">JACKETS</h1>
+          <span className = "subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className = "menu-item">
+        <div className = "content">
+          <h1 className = "title">SNEAKERS</h1>
+          <span className = "subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className = "menu-item">
+        <div className = "content">
+          <h1 className = "title">WOMENS</h1>
+          <span className = "subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className = "menu-item">
+        <div className = "content">
+          <h1 className = "title">MENS</h1>
+          <span className = "subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
   </div>
-=======
-
->>>>>>> ec8e55961d56523c69090d00d82dfb90e32be3ae
 )
+
+
+export default HomePage;
