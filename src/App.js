942948bcdc998623976ba3a76e19bff9bfa30d72
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-25 15:26:44 -0700

    added homepage import to app.js to properly render out site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from "./homepage.component"
+import HomePage from './homepage.component';
+
 
 
 class App extends Component {
