1ed9264a1bb8b664ef3d925be715b2382a59ca90
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-27 16:06:58 -0700

    updated routing with react-router-dom and routed to a Hats Page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+
+
 import './index.css';
+import App from './App';
+
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
