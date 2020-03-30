c2f7f3a498f21f0bd4ac8d67323072510e2b20c8
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-30 14:43:55 -0700

    created a new shop page and added SHOP_DATA for different items and styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 
 
 class App extends Component {
@@ -18,7 +15,8 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={ HomePage } />
-          <Route exact path='/hats' component={ HatsPage } />
+          <Route exact path='/shop' component={ ShopPage } />
+
         </Switch>
       </div>
     );
