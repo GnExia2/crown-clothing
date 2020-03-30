dad0e5c2c5796f0731b0bf6ef319dcde74320503
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-30 15:20:26 -0700

    created and styled header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component.jsx'
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component.jsx';
 
 
 
@@ -13,10 +14,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route exact path='/shop' component={ ShopPage } />
-
         </Switch>
       </div>
     );
