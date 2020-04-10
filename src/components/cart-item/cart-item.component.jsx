7b88a2a17fdadc285c3f562456bd413348a88fa2
Garrick Ngai <garrickdark@yahoo.com>
2020-04-10 15:45:03 -0700

    added remove item feature in cart and fixed minor typos withing code

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 import './cart-item.styles.scss';
@@ -8,9 +7,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <img src={imageUrl} alt='item' />
     <div className='item-details'>
       <span className='name'>{name}</span>
-      <span className='price'>
-        {quantity} x ${price}
-      </span>
+      <span className='price'>{quantity} x ${price}</span>
     </div>
   </div>
 );
